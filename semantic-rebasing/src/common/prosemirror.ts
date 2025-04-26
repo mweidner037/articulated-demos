59fdf9c4268056d453063f65d6f2d1d0a236f063
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2025-04-25 20:11:10 -0500

    start paragraphs

--- semantic-rebasing/src/common/prosemirror.ts
+++ semantic-rebasing/src/common/prosemirror.ts
@@ -2,7 +2,13 @@ import { Schema } from "prosemirror-model";
 
 export const schema = new Schema({
   nodes: {
-    doc: { content: "text*" },
-    text: { inline: true },
+    doc: { content: "block*" },
+    paragraph: {
+      content: "inline*",
+      group: "block",
+      parseDOM: [{ tag: "p" }],
+      toDOM: () => ["p", 0],
+    },
+    text: { inline: true, group: "inline" },
   },
 });
